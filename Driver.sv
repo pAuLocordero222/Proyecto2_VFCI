e93c5ed7cf48089237f7a9e9e876f8e0eb7fa5c9
pAuLocordero222 <88518574+pAuLocordero222@users.noreply.github.com>
2022-10-25 21:49:40 -0600

    update

---
+++ Driver.sv
@@ -0,0 +1,10 @@
+class driver extends uvm_driver;
+    `uvm_component_utils(driver)
+
+    function new (string name = "driver", uvm_component parent = null);
+        super.new (name, parent);
+    endfunction
+
+    \\Code
+
+endclass
---
+++ Interface.sv
@@ -0,0 +1,9 @@
+interface dut_if;
+    logic [2:0] r_mode;
+    logic [31:0] fp_X;
+    logic [31:0] fp_Y;
+    logic [31:0] fp_Z;
+    logic ovrf;
+    logic udrf;
+    
+endinterface
