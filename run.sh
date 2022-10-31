e3498962c136594a12691bc24159c4fb8354de4d
Paulo <88518574+pAuLocordero222@users.noreply.github.com>
2022-10-30 22:54:31 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 source synopsys_tools.sh;
 rm -rfv ls |grep -v ".*\.sv\|.*\.sh";
 
-vcs -Mupdate Testbench.sv -o salida -full64 -debug_all -sverilog -l log_test +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert;
+vcs -Mupdate Top.sv -o salida -full64 -sverilog -kdb -debug_acc+all -debug_region+cell+encrypt -l log_test -ntb_opts uvm +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert
 
-./salida -cm line+tgl+cond+fsm+branch+assert;
+
+./salida +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNME=test1 +ntb_random_seed=1
 dve -full64 -covdir salida.vdb &
