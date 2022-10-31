c25e68e878e691b86a731d42e100ef833dc62d8e
pAuLocordero222 <88518574+pAuLocordero222@users.noreply.github.com>
2022-10-30 22:15:04 -0600

    Update Environment.sv

--- Environment.sv
+++ Environment.sv
@@ -16,6 +16,6 @@ class environment extends uvm_env;
 
   virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
-    a0.m0.mon_analysis_port.connect(sb0.m_analysis_imp);
+    //a0.m0.mon_analysis_port.connect(sb0.m_analysis_imp);
   endfunction
 endclass
