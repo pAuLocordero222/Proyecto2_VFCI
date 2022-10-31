6af967df3a7c9a1b428c2f5143395bb7ff708f11
pAuLocordero222 <88518574+pAuLocordero222@users.noreply.github.com>
2022-10-30 21:56:07 -0600

    Update Environment.sv

--- Environment.sv
+++ Environment.sv
@@ -11,7 +11,7 @@ class environment extends uvm_env;
     virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     a0 = agent::type_id::create("a0",this);
-    sb0 = scoreboard::type_id::create("sb0",this);
+    //sb0 = scoreboard::type_id::create("sb0",this);
   endfunction
 
   virtual function void connect_phase(uvm_phase phase);
