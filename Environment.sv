9b07114b917b06cf2ff7e0e63d7902bef49ec194
Luis <luis.sulecio@gmail.com>
2022-10-30 23:20:43 -0600

    a

--- Environment.sv
+++ Environment.sv
@@ -6,16 +6,16 @@ class environment extends uvm_env;
   endfunction
 
   agent  a0;
-  //scoreboard sb0;
+  scoreboard sb0;
   
     virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     a0 = agent::type_id::create("a0",this);
-    //sb0 = scoreboard::type_id::create("sb0",this);
+    sb0 = scoreboard::type_id::create("sb0",this);
   endfunction
 
   virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
-    //a0.m0.mon_analysis_port.connect(sb0.m_analysis_imp);
+    a0.m0.mon_analysis_port.connect(sb0.m_analysis_imp);
   endfunction
 endclass
--- Scoreboard.sv
+++ Scoreboard.sv
@@ -1,4 +1,16 @@
-class scoreboard extends uvm?scoreboard;
+class scoreboard extends uvm_scoreboard;
+  `uvm_component_utils(scoreboard)
     
+    function new(string name="scoreboard",uvm_component parent=null);
+        super.new(name, parent);
+    endfunction
 
+    virtual function void build_phase(uvm_phase phase);
+        
+      	super.build_phase(phase);
+      
+        m_analysis_imp = new("m_analysis_imp",this);
+
+    endfunction
+    
 endclass
