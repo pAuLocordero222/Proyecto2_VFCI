4337948a89f150e4a94a5b2b8d393d83760133c3
Paulo <88518574+pAuLocordero222@users.noreply.github.com>
2022-11-21 01:10:48 -0600

    update

--- DUT_MUL.sv
+++ DUT_MUL.sv
@@ -755,8 +755,8 @@ endmodule
 
 module NET(
   input nan, inf, zer,
-  input [31:0]Z,
-  output [31:0]fp_Z);
+  input reg [31:0]Z,
+  output reg [31:0]fp_Z);
   
   //assign fp_Z = nan ? 32'h7fc00000 : (inf ? {Z[31], 8'hff, 23'b0} : (zer ? {Z[31], 8'h00, 23'b0} : Z));
 
