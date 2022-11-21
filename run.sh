83d7d909038dff4662164a04fd4520498029fc6f
pAuLocordero222 <paulocordero@estudiantec.cr>
2022-11-20 19:23:28 -0600

    update

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ vcs -Mupdate Top.sv -o salida -full64 -sverilog -kdb -debug_acc+all -debug_regio
 
 
 ./salida +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNME=test1 +ntb_random_seed=12
+./salida +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNME=test1 +ntb_random_seed=14
+./salida +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNME=test1 +ntb_random_seed=51
 
 ./salida -cm line+tgl+cond+fsm+branch+assert;
 dve -full64 -covdir salida.vdb &
